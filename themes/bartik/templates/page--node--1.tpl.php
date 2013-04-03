75903f44c3b36fd84c0ce2080bc66d5c31ba622c
Jon Pugh <jon@thinkdrop.net>
2013-04-03 16:39:22 -0300

    Update and rename page--node--1.tpl.php to page--node--temp.tpl.php

--- themes/bartik/templates/page--node--1.tpl.php
+++
@@ -1,4 +0,0 @@
-<h1>GOOD DAY SUNSHINE!</h1>
-<p>Workin on a splash page...</p>
-
-<scroll>ITS GOING TO ROCK!</scroll>
---
+++ themes/bartik/templates/page--node--temp.tpl.php
@@ -0,0 +1,15 @@
+<style>
+h1 {
+  text-align: center;
+  font-size: 10em;
+}
+p {
+  font-size: 5em;
+  font-style: italic;
+}
+</style>
+
+
+
+<h1>GOOD DAY SUNSHINE!</h1>
+<p>Workin on a splash page...</p>
